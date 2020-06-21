99438858b6b2489a6d09cb15a636995b6dc0253d
jasonmichael45 <jasonmichael45@hotmail.com>
2020-06-21 16:12:17 -0700

    added textfields

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+function handleSubmit(){
+
+}
+
+function Form() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+      <div>
+        <TextField 
+          required 
+          id="standard-required" 
+          label="Required" 
+          helperText="Name"
+          InputLabelProps={{
+            shrink: true,
+          }}  
+          variant="outlined"
+
+        />
+        </div>
+      <div>
+        <TextField 
+          required 
+          id="standard-required" 
+          label="Required" 
+          helperText="Email"
+          InputLabelProps={{
+            shrink: true,
+          }}   
+          variant="outlined"
+
+        />
+      </div>
+      <div>
+      <TextField
+          required
+          id="standard-multiline-static"
+          label="Required"
+          multiline
+          rows={4}
+          style={{ margin: 8 }}
+          helperText="Message"
+          margin="normal"
+          placeholder=""
+          InputLabelProps={{
+            shrink: true,
+          }} 
+          variant="outlined"
+
+        />
+      </div>
+
+    
+    </form>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form/>
     </div>
   );
 }
